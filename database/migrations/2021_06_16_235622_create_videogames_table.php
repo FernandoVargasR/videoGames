59cdae2c10d801da93f38031d0da30c08ed763f5
Mariam de Jesus Delgado Zerquera <mariam.delgado4258@alumnos.udg.mx>
2021-06-16 19:49:07 -0500

    Creación de tabla mediante migración y creación de modelo

---
+++ database/migrations/2021_06_16_235622_create_videogames_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideogamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videogames', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 100);
+            $table->string('categoria', 100);
+            $table->string('plataforma', 50);
+            $table->float('precio', 8,2);
+            $table->string('portada', 1024);
+            $table->string('descripcion',1000);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videogames');
+    }
+}
